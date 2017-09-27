49d101ce7abb9a6ade941c0329e247545ae2a170
Henrique Oliveira <haooliveira@gmail.com>
2017-09-26 21:04:35 -0300

    Improved variables

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 import csv
 import sys
 
-f = open(sys.argv[1], 'rt')
+fileopen = open(sys.argv[1], 'rt')
+
 try:
-    reader = csv.reader(f)
+    reader = csv.reader(fileopen)
     for row in reader:
         print row
 finally:
-    f.close()
+    fileopen.close()
