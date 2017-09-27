65370a6cdd6f8f83394bf8a77a9381b830cfdff1
Henrique Oliveira <haooliveira@gmail.com>
2017-09-27 13:15:29 -0300

    Add modules and add sorting

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python
 import csv
 import sys
+import operator
+import pandas as pd
+from collections import Counter
+from collections import defaultdict
 
+
+contador = Counter()
+result = {}
 fileopen = open(sys.argv[1], 'rt')
 
 try:
-    reader = csv.DictReader(fileopen)
-    for rows in reader:
-        rows = str(rows["Estado"])
-        print rows
+    reader = csv.reader(fileopen)
+    next(reader, None)
+    sort = sorted(reader, key=operator.itemgetter(0)) 
+    for Estado, Cidade, Nome in sort:
+        if Estado in result:
+            result[Estado].append(Nome)
+        else:
+            result[Estado] = [Nome]
+    for row in sort:
+        Estado, Cidade, Nome = row[0], row[1], row[2]
+        contador[Estado] += 1
+    for firesult in sorted(contador.items()):
+        print firesult
 finally:
     fileopen.close()
