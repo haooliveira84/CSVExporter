9a46362cb7a0f972caacf8155ff4e494596409de
Henrique Antonio de Oliveira <henrique.oliveira@byseven.com.br>
2018-06-25 12:16:55 -0300

    Import libs

--- api.py
+++ api.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python
 # This Python file uses the following encoding: utf-8
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import operator
+import json
+
+import requests
+from flask import Flask, make_response, request, Response, jsonify
+from flask_httpauth import HTTPTokenAuth
+
+
+from collections import Counter
+
