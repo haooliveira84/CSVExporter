637c2031cdf09a012c5cd1092fb66241b0bebbdf
Henrique Antonio de Oliveira <henrique.oliveira@byseven.com.br>
2018-06-26 12:16:00 -0300

    Load modules

---
+++ app/tests/test_jsonreport.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# This Python file uses the following encoding: utf-8
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import logging
+import pytest
+
+from pytest_jsonreport.plugin import JSONReport
+
